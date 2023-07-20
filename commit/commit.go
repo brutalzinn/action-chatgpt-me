911240aeaf7205558d45f6158cb7cc44086a3a54
brutalzinn <robertocpaes@gmail.com>
2023-07-19 22:54:08 -0300

    testing if i can edit repo files at action container level

--- commit/commit.go
+++ commit/commit.go
@@ -3,9 +3,7 @@ package commit
 import "os/exec"
 
 func Commit(message string) {
-	exec.Command("git add .")
-	exec.Command("git commit -m '"+ message+"'")
+	exec.Command("git add -A")
+	exec.Command("git commit -m \" commit test message \" ")
 	exec.Command("git push")
 }
-
-

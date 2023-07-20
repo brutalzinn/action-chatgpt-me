478fdc047b1cb91fdecf256904aca5fc3f6b63a9
brutalzinn <robertocpaes@gmail.com>
2023-07-20 07:20:55 -0300

    " test"

--- commit/commit.go
+++ commit/commit.go
@@ -6,19 +6,19 @@ import (
 )
 
 func Add() {
-	_, err := exec.Command("bash", "-c", "git add .").Output()
+	_, err := exec.Command("git", "add", ".").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 func Commit(message string) {
-	_, err := exec.Command("bash", "-c", "git commit -m \" "+message+"\"").Output()
+	_, err := exec.Command("git commit -m \" " + message + "\"").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 func Push() {
-	_, err := exec.Command("bash", "-c", "git push").Output()
+	_, err := exec.Command("git push").Output()
 	if err != nil {
 		log.Fatal(err)
 	}

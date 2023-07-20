6d57d7ce86fb5a03c2e6b9c6f6012d41d2d0a1f2
brutalzinn <robertocpaes@gmail.com>
2023-07-19 22:26:07 -0300

    add envs

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	wakaTimeApiKey := os.Getenv("WAKATIME_API_KEY")
 	chatGptKey := os.Getenv("CHAT_GPT_API_KEY")
-	fmt.Println("No. you will not cant see any of my keys here. :)", wakaTimeApiKey, chatGptKey)
+	fmt.Println("No. you will not cant see any of my keys here. :) %s %s", wakaTimeApiKey, chatGptKey)
 }
